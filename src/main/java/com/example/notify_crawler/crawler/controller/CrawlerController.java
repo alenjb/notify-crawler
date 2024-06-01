e22310a92af817ff9b1567ac5ba0b5b8e6c4588b
alenjb <alenjb@naver.com>
2024-06-01 23:31:53 +0900

    #1 [fix] 크롤링 방법 병합
    - 공통 공지사항 크롤링 방법을 학과 공지사항 크롤링 방법과 같게 만들어 기존의 공통 공지사항만을 위한 코드는 제거하였음

--- src/main/java/com/example/notify_crawler/crawler/controller/CrawlerController.java
+++ src/main/java/com/example/notify_crawler/crawler/controller/CrawlerController.java
@@ -1,6 +1,5 @@
 package com.example.notify_crawler.crawler.controller;
 
-import com.example.notify_crawler.common.constants.CrawlerConstants;
 import com.example.notify_crawler.common.domain.NoticeType;
 import com.example.notify_crawler.crawler.service.CrawlerService;
 import com.example.notify_crawler.notice.domain.Notice;
@@ -19,10 +18,7 @@ import org.springframework.stereotype.Controller;
 import java.text.ParseException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Base64;
 import java.util.List;
-
-import static com.example.notify_crawler.crawler.service.CrawlerServiceImpl.getChromeOptions;
 
 @Controller
 @Slf4j
@@ -43,73 +39,19 @@ public class CrawlerController {
 
 
     /**
-     * 공통 공지사항의 새 글을 가져와 DB에 저장한다.
-     */
-    @Scheduled(cron = "0 */30 * * * *") // 매 30분마다 실행
-    @Transactional
-    @PostConstruct
-    public void getComNotice() throws InterruptedException, ParseException {
-        log.info("==== 새 공통 공지사항 크롤링 시작 시각: "+ String.valueOf(LocalDateTime.now())+"====");
-        ChromeOptions options = getChromeOptions();
-        // WebDriver 인스턴스 생성
-        WebDriver driver = new ChromeDriver(options);
-
-        // 로그인 아이디와 비밀번호 디코딩
-        String username = new String(Base64.getDecoder().decode("YWxlbmpi"));
-        String password = new String(Base64.getDecoder().decode("ZGx3amRxbHMxMiE="));
-
-        crawlerService.loginAndGoToComNoticePage(driver, username, password);
-        // 새 글의 개수
-        int newNoticeCount = crawlerService.getNewNoticeCount(
-                NoticeType.COM,
-                driver,
-                crawlerService.getLastTwoNotices(NoticeType.COM));
-            driver.quit();
-
-        // 새 글이 있으면
-        if(newNoticeCount > 0){
-            // 새 글들을 저장할 리스트
-            List<Notice> newNotices = new ArrayList<>();
-
-            // 새 글을 크롤링할 페이지 수
-            int newNoticePageCount = newNoticeCount / CrawlerConstants.CRAWLING_COM_NOTICE_SIZE_PER_PAGE;
-            // 새 글 수를 페이지 수로 나누었을 때 떨어지는 나머지 값
-            int newNoticeNumInLastPage = newNoticeCount % CrawlerConstants.CRAWLING_COM_NOTICE_SIZE_PER_PAGE;
-
-            // 페이지에서 일부만 크롤링해야하는 새 글들을 크롤링 해오기
-            List<Notice> newNoticesFromLastPage = crawlerService.getNewComNoticesByPageNum(username, password, newNoticePageCount + 1, driver);
-            for(int i=1; i<=newNoticeNumInLastPage; i++){
-                Notice notice = newNoticesFromLastPage.get(CrawlerConstants.CRAWLING_COM_NOTICE_SIZE_PER_PAGE - i);
-                newNotices.add(notice);
-            }
-            // 페이지를 통채로 가져올 수 있는 만큼 새 글을 크롤링 해오기
-            for(int i=newNoticePageCount; i>=1 ;i--){
-                List<Notice> notices = crawlerService.getNewComNoticesByPageNum(username, password, i, driver);
-                for (Notice notice : notices) {
-                    newNotices.add(notice);
-                }
-            }
-            // 가져온 새 공통 공지사항들 DB에 저장
-            crawlerService.saveNewComNotices(newNotices);
-            log.info("==== 새 공통 공지사항 저장 완료 시각: "+ String.valueOf(LocalDateTime.now())+"====");
-            kafkaProducer.sendMessage("공통 공지사항 저장 완료");
-        }
-    }
-
-    /**
-     * 학과 공지사항의 새 글을 가져와 DB에 저장한다.
+     * 공지사항의 새 글을 가져와 DB에 저장한다.
      * @throws InterruptedException
      * @throws ParseException
      */
     @Scheduled(cron = "0 */30 * * * *") // 매 30분마다 실행
     @Transactional
     @PostConstruct
-    public void getAllMajorNotice() throws InterruptedException, ParseException {
-        log.info("==== "+ "학과 새 공지사항 크롤링 시작 시각: "+ String.valueOf(LocalDateTime.now())+"====");
+    public void getAllNotice() throws InterruptedException, ParseException {
+        log.info("==== "+ "새 공지사항 크롤링 시작 시각: "+ String.valueOf(LocalDateTime.now())+"====");
         // Headless 모드로 Chrome 실행
         ChromeOptions options = new ChromeOptions();
         // Headless 모드 활성화
-        options.addArguments("--headless");
+//        options.addArguments("--headless");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--ignore-ssl-errors=yes");
         options.addArguments("--ignore-certificate-errors");
@@ -117,10 +59,8 @@ public class CrawlerController {
         // WebDriver 인스턴스 생성
         WebDriver driver = new ChromeDriver(options);
         NoticeType[] noticeTypes = NoticeType.values();
-        // 모든 학과 공지 사항에 대해 반복
+        // 모든 공지 사항에 대해 반복
         for (NoticeType noticeType : noticeTypes) {
-            // 공통 공지사항이면 지나가기
-            if(noticeType == NoticeType.COM) continue;
             // 새 글의 개수
             int newNoticeCount = crawlerService.getNewNoticeCount(
                     noticeType,
@@ -138,24 +78,22 @@ public class CrawlerController {
                 int newNoticeNumInLastPage = newNoticeCount % noticeType.getNoticeSizePerPage();
 
                 // 페이지에서 일부만 크롤링해야하는 새 글들을 크롤링 해오기
-                List<Notice> newNoticesFromLastPage = crawlerService.getNewMajorNoticesByPageNum(newNoticePageCount + 1, noticeType, driver);
+                List<Notice> newNoticesFromLastPage = crawlerService.getNewNoticesByPageNum(newNoticePageCount + 1, noticeType, driver);
                 for(int i=1; i<=newNoticeNumInLastPage; i++){
                     Notice notice = newNoticesFromLastPage.get(noticeType.getNoticeSizePerPage() - i);
                     newNotices.add(notice);
                 }
                 // 페이지를 통채로 가져올 수 있는 만큼 새 글을 크롤링 해오기
                 for(int i=newNoticePageCount; i>=1 ;i--){
-                    List<Notice> notices = crawlerService.getNewMajorNoticesByPageNum(i, noticeType, driver);
+                    List<Notice> notices = crawlerService.getNewNoticesByPageNum(i, noticeType, driver);
                     for (Notice notice : notices) {
                         newNotices.add(notice);
                     }
                 }
                 // 가져온 새 공통 공지사항들 DB에 저장
-                crawlerService.saveNewMajorNotices(newNotices, noticeType);
-                log.info("==== "+ noticeType + "학과 새 공지사항 저장 완료 시각: "+ String.valueOf(LocalDateTime.now())+"====");
-                kafkaProducer.sendMessage("학과 공지사항 저장 완료");
-
-
+                crawlerService.saveNewNotices(newNotices, noticeType);
+                log.info("==== "+ noticeType + " 새 공지사항 저장 완료 시각: "+ String.valueOf(LocalDateTime.now())+"====");
+                kafkaProducer.sendMessage("공지사항 저장 완료");
             }
         }
     }
