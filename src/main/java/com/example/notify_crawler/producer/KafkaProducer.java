5d1f92a14a1f662f8738e3ed9367f1964be0a3a6
alenjb <alenjb@naver.com>
2024-06-02 22:06:47 +0900

    #5 [fix] producer 메서드 수정
    - List<Notice>를 직렬화해서 보내도록 수정

--- src/main/java/com/example/notify_crawler/producer/KafkaProducer.java
+++ src/main/java/com/example/notify_crawler/producer/KafkaProducer.java
@@ -1,18 +1,34 @@
 package com.example.notify_crawler.producer;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
+import com.example.notify_crawler.notice.domain.Notice;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Properties;
 
 @Service
 public class KafkaProducer {
-
     private static final String TOPIC = "notify-crawler-topic";
 
-    @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
+    public static void produce(List<Notice> newNotices) {
+        // Kafka Producer 설정
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "15.164.34.15:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, NoticeSerializer.class.getName());
 
-    public void sendMessage(String message) {
-        kafkaTemplate.send(TOPIC, message);
+        // Kafka Producer 생성
+        org.apache.kafka.clients.producer.KafkaProducer<String, List<Notice>> producer = new org.apache.kafka.clients.producer.KafkaProducer<>(props);
+
+        // Kafka에 메시지 보내기
+        ProducerRecord<String, List<Notice>> record = new ProducerRecord<>(TOPIC, newNotices);
+        producer.send(record);
+
+        // Producer 종료
+        producer.flush();
+        producer.close();
     }
 }
