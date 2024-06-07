238c3a02d75743f3ee632099238c9cfef7e063d8
alenjb <alenjb@naver.com>
2024-06-07 19:54:55 +0900

    #6 [feat] 공지사항 카테고리 추가
    - 카테고리 별로 크롤링 하기 위해 공지사항 카테고리 enum을 추가함

--- src/main/java/com/example/notify_crawler/notice/domain/Notice.java
+++ src/main/java/com/example/notify_crawler/notice/domain/Notice.java
@@ -1,6 +1,7 @@
 package com.example.notify_crawler.notice.domain;
 
 import com.example.notify_crawler.common.domain.BaseTimeEntity;
+import com.example.notify_crawler.common.domain.NoticeCategory;
 import com.example.notify_crawler.common.domain.NoticeType;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -33,6 +34,8 @@ public class Notice extends BaseTimeEntity implements Serializable {
 
     private Date noticeDate;
 
+    @Enumerated(EnumType.STRING)
+    private NoticeCategory noticeCategory;
 
     @Enumerated(EnumType.STRING)
     private NoticeType noticeType;
